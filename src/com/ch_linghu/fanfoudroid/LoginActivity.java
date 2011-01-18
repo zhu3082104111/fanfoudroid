1e5404aa397bc483e0a63f9ed4eb408bab46cd96
Sam Lau <lds2012@gmail.com>
2011-01-18 17:35:53 +0800

    compress image

---
+++ src/com/ch_linghu/fanfoudroid/LoginActivity.java
@@ -0,0 +1,274 @@
+/*
+ * Copyright (C) 2009 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ch_linghu.fanfoudroid;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.text.method.LinkMovementMethod;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.ch_linghu.fanfoudroid.helper.Preferences;
+import com.ch_linghu.fanfoudroid.helper.Utils;
+import com.ch_linghu.fanfoudroid.http.HttpAuthException;
+import com.ch_linghu.fanfoudroid.http.HttpRefusedException;
+import com.ch_linghu.fanfoudroid.weibo.WeiboException;
+import com.google.android.photostream.UserTask;
+
+public class LoginActivity extends Activity {
+    private static final String TAG = "LoginActivity";
+    private static final String SIS_RUNNING_KEY = "running";
+
+    private String mUsername;
+    private String mPassword;
+
+    // Views.
+    private EditText mUsernameEdit;
+    private EditText mPasswordEdit;
+    private TextView mProgressText;
+    private Button mSigninButton;
+    private ProgressDialog dialog;
+
+    // Preferences.
+    private SharedPreferences mPreferences;
+
+    // Tasks.
+    private UserTask<String, String, Boolean> mLoginTask;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "onCreate");
+        super.onCreate(savedInstanceState);
+
+        // No Title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        requestWindowFeature(Window.FEATURE_PROGRESS);
+
+        mPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        setContentView(R.layout.login);
+
+        // TextView中嵌入HTML链接
+        TextView registerLink = (TextView) findViewById(R.id.register_link);
+        registerLink.setMovementMethod(LinkMovementMethod.getInstance());
+
+        mUsernameEdit = (EditText) findViewById(R.id.username_edit);
+        mPasswordEdit = (EditText) findViewById(R.id.password_edit);
+        // mUsernameEdit.setOnKeyListener(enterKeyHandler);
+        mPasswordEdit.setOnKeyListener(enterKeyHandler);
+
+        mProgressText = (TextView) findViewById(R.id.progress_text);
+        mProgressText.setFreezesText(true);
+        mSigninButton = (Button) findViewById(R.id.signin_button);
+
+        if (savedInstanceState != null) {
+            if (savedInstanceState.containsKey(SIS_RUNNING_KEY)) {
+                if (savedInstanceState.getBoolean(SIS_RUNNING_KEY)) {
+                    Log.i(TAG, "Was previously logging in. Restart action.");
+                    doLogin();
+                }
+            }
+        }
+
+        mSigninButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                doLogin();
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.i(TAG, "onDestory");
+        if (mLoginTask != null && mLoginTask.getStatus() == UserTask.Status.RUNNING) {
+            mLoginTask.cancel(true);
+        }
+
+        // dismiss dialog before destroy
+        // to avoid android.view.WindowLeaked Exception
+        if (dialog != null) {
+            dialog.dismiss();
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.i(TAG, "onStop");
+        // TODO Auto-generated method stub
+        super.onStop();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        if (mLoginTask != null
+                && mLoginTask.getStatus() == UserTask.Status.RUNNING) {
+            // If the task was running, want to start it anew when the
+            // Activity restarts.
+            // This addresses the case where you user changes orientation
+            // in the middle of execution.
+            outState.putBoolean(SIS_RUNNING_KEY, true);
+        }
+    }
+
+    // UI helpers.
+
+    private void updateProgress(String progress) {
+        mProgressText.setText(progress);
+    }
+
+    private void enableLogin() {
+        mUsernameEdit.setEnabled(true);
+        mPasswordEdit.setEnabled(true);
+        mSigninButton.setEnabled(true);
+    }
+
+    private void disableLogin() {
+        mUsernameEdit.setEnabled(false);
+        mPasswordEdit.setEnabled(false);
+        mSigninButton.setEnabled(false);
+    }
+
+    // Login task.
+
+    private void doLogin() {
+        mUsername = mUsernameEdit.getText().toString();
+        mPassword = mPasswordEdit.getText().toString();
+        
+        if (!Utils.isEmpty(mUsername) & !Utils.isEmpty(mPassword) ) {
+            mLoginTask = new LoginTask().execute(mUsername, mPassword);
+        } else {
+            updateProgress(getString(R.string.login_status_null_username_or_password));
+        }
+    }
+
+    private void onLoginBegin() {
+        disableLogin();
+        dialog = ProgressDialog.show(LoginActivity.this, "",
+                getString(R.string.login_status_logging_in), true);
+    }
+
+    private void onLoginSuccess() {
+        dialog.dismiss();
+        updateProgress("");
+        mUsernameEdit.setText("");
+        mPasswordEdit.setText("");
+
+        Log.i(TAG, "Storing credentials.");
+        SharedPreferences.Editor editor = mPreferences.edit();
+        editor.putString(Preferences.USERNAME_KEY, mUsername);
+        editor.putString(Preferences.PASSWORD_KEY, mPassword);
+        editor.commit();
+
+        TwitterApplication.mApi.setCredentials(mUsername, mPassword);
+
+        Intent intent = getIntent().getParcelableExtra(Intent.EXTRA_INTENT);
+        String action = intent.getAction();
+
+        if (intent.getAction() == null || !Intent.ACTION_SEND.equals(action)) {
+            // We only want to reuse the intent if it was photo send.
+            // Or else default to the main activity.
+            intent = new Intent(this, TwitterActivity.class);
+        }
+
+        startActivity(intent);
+        finish();
+    }
+
+    private void onLoginFailure(String reason) {
+        Toast.makeText(this, reason, Toast.LENGTH_SHORT).show();
+        dialog.dismiss();
+        enableLogin();
+    }
+
+    private class LoginTask extends UserTask<String, String, Boolean> {
+        
+        private String msg = getString(R.string.login_status_failure);
+        
+        @Override
+        public void onPreExecute() {
+            onLoginBegin();
+        }
+
+        @Override
+        public Boolean doInBackground(String... params) {
+            publishProgress(getString(R.string.login_status_logging_in) + "...");
+
+            try {
+                TwitterApplication.mApi.login(params[0], params[1]);
+            } catch (WeiboException e) {
+                Log.e(TAG, e.getMessage(), e);
+
+                Throwable cause = e.getCause(); // Maybe null
+                if (cause instanceof HttpAuthException) {
+                    // Invalid userName/password
+                    msg = ((HttpRefusedException) cause).getError().getMessage();
+                } else {
+                    msg = getString(R.string.login_status_network_or_connection_error);
+                }
+                publishProgress(msg);
+                return false;
+            }
+            return true;
+        }
+
+        @Override
+        public void onProgressUpdate(String... progress) {
+            updateProgress(progress[0]);
+        }
+
+        @Override
+        public void onPostExecute(Boolean result) {
+            if (isCancelled()) {
+                return;
+            }
+
+            if (result) {
+                onLoginSuccess();
+            } else {
+                onLoginFailure(msg);
+            }
+        }
+    }
+    
+    private View.OnKeyListener enterKeyHandler = new View.OnKeyListener() {
+        public boolean onKey(View v, int keyCode, KeyEvent event) {
+            if (keyCode == KeyEvent.KEYCODE_ENTER
+                    || keyCode == KeyEvent.KEYCODE_DPAD_CENTER) {
+                if (event.getAction() == KeyEvent.ACTION_UP) {
+                    doLogin();
+                }
+                return true;
+            }
+            return false;
+        }
+    };
+
+}
---
+++ src/com/ch_linghu/fanfoudroid/TwitterApplication.java
@@ -0,0 +1,83 @@
+package com.ch_linghu.fanfoudroid;
+
+import java.util.HashSet;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.preference.PreferenceManager;
+
+import com.ch_linghu.fanfoudroid.data.db.TwitterDbAdapter;
+import com.ch_linghu.fanfoudroid.helper.ImageManager;
+import com.ch_linghu.fanfoudroid.helper.Preferences;
+import com.ch_linghu.fanfoudroid.weibo.User;
+import com.ch_linghu.fanfoudroid.weibo.Weibo;
+
+public class TwitterApplication extends Application {
+  
+  public static final String TAG = "TwitterApplication";
+  
+  public static ImageManager mImageManager;
+  public static TwitterDbAdapter mDb; 
+  public static Weibo mApi; // new API
+  public static Context mContext;
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+    mContext = this.getApplicationContext();
+    mImageManager = new ImageManager(this);
+    mApi = new Weibo();
+    mDb = new TwitterDbAdapter(this);
+    mDb.open();
+    
+    SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);        
+    String username = preferences.getString(Preferences.USERNAME_KEY, "");
+    String password = preferences.getString(Preferences.PASSWORD_KEY, "");
+    
+    if (Weibo.isValidCredentials(username, password)) {
+        mApi.setCredentials(username, password); // Setup API and HttpClient 
+    }
+  }
+
+  @Override
+  public void onTerminate() {
+    cleanupImages();
+    mDb.close();
+//    Toast.makeText(this, "exit app", Toast.LENGTH_LONG);
+    
+    super.onTerminate();
+  }
+  
+  private void cleanupImages() {
+    HashSet<String> keepers = new HashSet<String>();
+    
+    Cursor cursor = mDb.fetchAllTweets(TwitterDbAdapter.TABLE_TWEET);
+    
+    if (cursor.moveToFirst()) {
+      int imageIndex = cursor.getColumnIndexOrThrow(
+          TwitterDbAdapter.KEY_PROFILE_IMAGE_URL);
+      do {
+        keepers.add(cursor.getString(imageIndex));
+      } while (cursor.moveToNext());
+    }
+    
+    cursor.close();
+    
+    cursor = mDb.fetchAllDms();
+    
+    if (cursor.moveToFirst()) {
+      int imageIndex = cursor.getColumnIndexOrThrow(
+          TwitterDbAdapter.KEY_PROFILE_IMAGE_URL);
+      do {
+        keepers.add(cursor.getString(imageIndex));
+      } while (cursor.moveToNext());
+    }
+    
+    cursor.close();
+    
+    mImageManager.cleanup(keepers);
+  }
+}
