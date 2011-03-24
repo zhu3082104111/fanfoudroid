06364bb8b65ffa31295f39663f957d2592b2377f
phoenix <phoenix@localhost>
2011-03-24 23:10:29 +0800

    ?????????1.???UserActivity??UserTimelineActivity????????????ProfileActivity,MenuDialog.2.?????????????????????????.3.?????????????????????????????

---
+++ src/com/ch_linghu/fanfoudroid/PreferencesActivity.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2009 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ch_linghu.fanfoudroid;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceScreen;
+import android.util.Log;
+
+import com.ch_linghu.fanfoudroid.helper.Preferences;
+import com.ch_linghu.fanfoudroid.http.HttpClient;
+
+public class PreferencesActivity extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // TODO: is this a hack?
+        setResult(RESULT_OK);
+
+        addPreferencesFromResource(R.xml.preferences);
+        
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        
+        // Set up a listener whenever a key changes
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen,
+            Preference preference) {
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+    
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+            String key) {
+        
+        if ( key.equalsIgnoreCase(Preferences.NETWORK_TYPE) ) {
+            HttpClient httpClient = TwitterApplication.mApi.getHttpClient();
+            String type =  sharedPreferences.getString(Preferences.NETWORK_TYPE, "");
+            
+            if (type.equalsIgnoreCase(getString(R.string.pref_network_type_cmwap))) {
+                Log.i("LDS", "Set proxy for cmwap mode.");
+                httpClient.setProxy("10.0.0.172", 80, "http");
+            } else {
+                Log.i("LDS", "No proxy.");
+                httpClient.removeProxy();
+            }
+        }
+        
+    }
+    
+
+}
